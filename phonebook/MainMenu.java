fbd59a928a8da0e128f7264885dc06060862c65f
GretaLorraine <67609921+GretaLorraine@users.noreply.github.com>
2020-07-22 13:17:00 -0500

    Add files via upload

---
+++ phonebook/MainMenu.java
@@ -0,0 +1,15 @@
+package com.phonebook;
+
+import java.util.Scanner;
+
+public class MainMenu {
+
+	
+	static Scanner myScanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		Menu.menuStart();
+		
+	}
+
+}
